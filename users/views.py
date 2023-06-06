6891dacd8e5af28ddd8acb1e0b7cf51fe0766c83
sumit <sumitsantosh555@gmail.com>
2023-06-06 13:43:31 +0530

    login and logout are complete

--- users/views.py
+++ users/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render , redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from .forms import UserRegisterForm
 
 def register(requset):
     if requset.method == 'POST':
-        form = UserCreationForm(requset.POST)
+        form = UserRegisterForm(requset.POST)
         if form.is_valid():
+            form.save()
             username = form.cleaned_data.get('username')
             messages.success(requset , f'Account created for {username}!')
-            return redirect('blog-home')
+            return redirect('login')
+        
     else:
-        form = UserCreationForm()
+        form = UserRegisterForm()
     return render(requset , 'users/register.html' , {'form':form})
+
